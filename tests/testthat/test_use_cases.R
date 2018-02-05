777db5cb2185854b2803da3ee1d4be8c5c79b8b8
Wolfgang Vollmer <wo.volmer@online.de>
2018-02-05 17:59:06 +0100

    fixes "issue #4": Test infrastructure and first tests generated

--- tests/testthat/test_use_cases.R
+++ tests/testthat/test_use_cases.R
@@ -9,46 +9,48 @@ data <- prostate
 max_formula <- lpsa ~ (. - train)
 model <- regsubsets(max_formula, data=data, nbest = 1, nvmax = 8,
   really.big = TRUE)
+model$call <- quote(regsubsets(max_formula, data = data, 
+  nbest = 1, nvmax = 8, really.big = TRUE))
+  # 'model$call <- ...' needed, otherwise test cases are not running 
+  #  with devtools::test() or covr::package_coverage() => throwing error:
+  #  object 'code' not found
+  #   reason: model$call when refit.regsubsets is run under devtools::test 
+  # does not have the expected structure
 
-## test cases are running within this test_use_cases.R file
-#  but test file not running with devtools::test() or covr::package_coverage()
-#        throws error: object 'code' not found 
-#   reason not yet found si  => commented out the test cases
+test_that("get_stability_paths with prostate data - subsample", {
+  expect_is(
+    get_stability_paths(model, data, reps = 10)
+    , c("stabpath", "matrix"))
+  expect_named(
+    get_stability_paths(model, data, reps = 10)[5, ]
+    , c("lcavol", "lweight", "age", "lbph", "svi", "lcp", "gleason", "pgg45"))
+})
 
-# test_that("get_stability_paths with prostate data - subsample", {
-#   expect_is(
-#     get_stability_paths(model, data, reps = 10)
-#     , c("stabpath", "matrix"))
-#   expect_named(
-#     get_stability_paths(model, data, reps = 10)[5, ]
-#     , c("lcavol", "lweight", "age", "lbph", "svi", "lcp", "gleason", "pgg45"))
-# })
+set.seed(20141020)
+s <- get_stability_paths(model, data, reps = 10)[5, ]
 
-# set.seed(20141020)
-# s <- get_stability_paths(model, data, reps = 10)[5, ]
-# 
-# test_that("get_stability_paths with prostate data - subsample, row values", {
-#   expect_equal(
-#     unname(s)
-#     , c(1.0, 0.8, 0.3, 0.7, 0.9, 0.1, 0.0, 0.2))
-# })
+test_that("get_stability_paths with prostate data - subsample, row values", {
+  expect_equal(
+    unname(s)
+    , c(1.0, 0.8, 0.3, 0.7, 0.9, 0.1, 0.0, 0.2))
+})
 
-# context("stabpath::get_stability_paths (w/ bootstrap method) - test of use cases ")
-# 
-# test_that("get_stability_paths with prostate data - bootstrap", {
-#   expect_is(
-#     get_stability_paths(model, data, reps = 10, method = "bootstrap")
-#     , c("stabpath", "matrix"))
-#   expect_named(
-#     get_stability_paths(model, data, reps = 10, method = "bootstrap")[5, ]
-#     , c("lcavol", "lweight", "age", "lbph", "svi", "lcp", "gleason", "pgg45"))
-# })
+context("stabpath::get_stability_paths (w/ bootstrap method) - test of use cases ")
 
-# set.seed(20141020)
-# b <-   get_stability_paths(model, data, reps = 10, method = "bootstrap")[5, ]
-# test_that("get_stability_paths with prostate data - bootstrap, row values", {
-#   expect_equal(
-#     unname(b)
-#     , c(1.0, 0.7, 0.4, 0.4, 0.9, 0.2, 0.2, 0.2))
-# })
+test_that("get_stability_paths with prostate data - bootstrap", {
+  expect_is(
+    get_stability_paths(model, data, reps = 10, method = "bootstrap")
+    , c("stabpath", "matrix"))
+  expect_named(
+    get_stability_paths(model, data, reps = 10, method = "bootstrap")[5, ]
+    , c("lcavol", "lweight", "age", "lbph", "svi", "lcp", "gleason", "pgg45"))
+})
+
+set.seed(20141020)
+b <-   get_stability_paths(model, data, reps = 10, method = "bootstrap")[5, ]
+test_that("get_stability_paths with prostate data - bootstrap, row values", {
+  expect_equal(
+    unname(b)
+    , c(1.0, 0.7, 0.4, 0.4, 0.9, 0.2, 0.2, 0.2))
+})
 
